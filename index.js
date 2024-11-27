b62ffcb9a7c4d5dec3b5efb486ff9e816a59c75b
osw00817 <42710245+osw00817@users.noreply.github.com>
2024-11-27 15:47:23 +0900

    보안 업데이트

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const TelegramBot = require('node-telegram-bot-api');
-
-
+require('dotenv').config();
 // log 남기기
-// file handling
-// list 
+
+/*
+
+파일 제목 형식
+과목_교수_과제명/수업자료명
+
+*/
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '7668501773:AAFYORZyVJfeXkPH3OTArsu0GhvvjP8wJ_A';
+const token = process.env.BOT_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -28,13 +32,14 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  console.log(msg.chat.id);
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, `${msg.chat}`);
-  //bot.sendMessage(-1002274686707, "test");
- // bot.sendDocument(-1002274686707,"");
+  if(msg.document) {
+    console.log(msg.document.file_name);
+    bot.sendDocument(process.env.CHANNEL_ID,msg.document.file_id);
+    bot.sendMessage(process.env.LOG_ID, `${msg.chat.first_name}${msg.chat.last_name}(${msg.chat.username})이/가 ${msg.document.file_name}를 아카이브에 공유하셧습니다.`);
+  }
 });
+
+
 
 bot.on('file',(msg) => {
     bot.sendMessage(chatId, `file`);
